035d1430f6fcf31307ce5e3a6242dea91095d3a1
Donald Lee <donaldlee50@gmail.com>
2024-08-03 22:56:37 -0500

    Start editing landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,6 @@ import { Testimonials } from '@/components/Testimonials'
 export default function Home() {
   return (
     <>
-      <Header />
       <main>
         <Hero />
         <PrimaryFeatures />
